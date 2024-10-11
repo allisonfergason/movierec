deadfb06c8a3c4045036fe8f0a97b47281256d68
Allison <allisonfergason@gmail.com>
2024-10-11 12:46:01 -0400

    first commit

---
+++ src/main/java/com/inocu/movierec/MovierecApplication.java
@@ -0,0 +1,13 @@
+package com.inocu.movierec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovierecApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovierecApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/inocu/movierec/MovierecApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inocu.movierec;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovierecApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
